92c71420ec923fedc65353bce1ac199959b89cdb
Alexis Raya <aar335@drexel.edu>
2024-11-09 21:56:51 -0500

    add button hold animation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,5 +3,6 @@
 <ul>
     <li><a href="./exploration/login-system">Login System</a></li>
     <li><a href="./exploration/notifications">Notifications</a></li>
+    <li><a href="./exploration/press-and-hold">Press and Hold</a></li>
 </ul>
 
