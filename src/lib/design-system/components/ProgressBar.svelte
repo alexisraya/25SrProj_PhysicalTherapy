f2d9b07e9fcb9a41a90b890097c6907eecaebdd7
Dane LeGassick <danelegassick@gmail.com>
2025-03-28 13:42:33 -0400

    Progress bar animation
    Most recent completed progress line fills up upon loading.

--- src/lib/design-system/components/ProgressBar.svelte
+++ src/lib/design-system/components/ProgressBar.svelte
@@ -7,7 +7,7 @@
 
 <div class="progress_bar" style="display: grid; grid-template-columns: repeat({totalExercises}, 1fr);">
     {#each Array(completedExercises).fill(0) as _, i}
-        <div class="progress_bar--item"></div>
+        <div class="progress_bar--item {i === completedExercises - 1 ? 'animate-fill' : ''}"></div>
     {/each}
     {#each Array(remainingExercises).fill(0) as _, i}
         <div class="progress_bar--item empty"></div>
@@ -31,5 +31,18 @@
     }
     .empty {
         opacity: 40%;
+    }
+    .animate-fill {
+        transform: scaleX(0); /* Start from 0 */
+        transform-origin: left; /* Expand from the left */
+        animation: fillAnimation 1s ease-in-out forwards;
+    }
+    @keyframes fillAnimation {
+        from {
+            transform: scaleX(0);
+        }
+        to {
+            transform: scaleX(1);
+        }
     }
 </style>
